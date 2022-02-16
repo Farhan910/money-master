426ce654fe7f0968b49fc40aee803608da05b98c
Farhan910 <farhanfaruk221@gmail.com>
2022-02-17 03:42:00 +0600

    3rd

--- script.js
+++ script.js
@@ -1,10 +1,88 @@
-const expenses = document.getElementById('calc-button').addEventListener('click',function(){
+
+function getUpdate(click){
+
+
     
 
-    const foodPrice = document.getElementById('food-price').value;
-    const rentPrice = document.getElementById('rent-price').value;
-    const clothesPrice = document.getElementById('clothes-price').value;
 
-    const totalPrice = Number(foodPrice.value)  + Number(rentPrice.value) + Number(clothesPrice.value);
-    console.log(totalPrice.value)
-})
+
+        document.getElementById(click).addEventListener('click',function(){
+
+
+           
+                
+            
+
+
+            const foodPrice = document.getElementById('food-price').value;
+            const rentPrice = document.getElementById('rent-price').value;
+            const clothesPrice = document.getElementById('clothes-price').value;
+    
+        
+            const totalPrice = parseFloat(foodPrice)  + parseFloat(rentPrice) + parseFloat(clothesPrice);
+            
+        
+        
+            let expensesPrice = document.getElementById('expenses-price').innerText = totalPrice;
+            
+            let  income = document.getElementById('income-money').value;
+    
+            income =   parseFloat(income) - parseFloat(expensesPrice);
+    
+            let  balancePrice = document.getElementById('balance-price')  ;
+    
+            balancePrice.innerText =  income ;
+
+            
+    
+            document.getElementById(click).addEventListener('click',function(){
+    
+            })
+        
+    
+            let saveInput = document.getElementById('button-input').value;
+            const savingAmount = document.getElementById('saving-amount');
+    
+            savingAmount.innerText = saveInput / 10;
+            
+    
+            let remainingAmount = document.getElementById('remaining-amount').innerText  = parseFloat( balancePrice) - parseFloat( savingAmount) ;
+            
+    
+    
+        
+            
+    
+            
+            
+            
+            
+            
+               
+        })
+
+    
+
+    
+    
+
+    
+     
+
+}
+
+
+
+getUpdate('calc-button')  
+getUpdate('button-save')
+
+
+    
+
+
+
+    
+
+
+
+
