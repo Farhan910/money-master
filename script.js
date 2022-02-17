9f8a82eb50125b6d7b336b48b126ac339c437711
Farhan910 <farhanfaruk221@gmail.com>
2022-02-17 16:15:25 +0600

    4rth

--- script.js
+++ script.js
@@ -1,88 +1,37 @@
+function getUpdate(click) {
+  document.getElementById(click).addEventListener("click", function () {
+    const foodPrice = document.getElementById("food-price").value;
+    const rentPrice = document.getElementById("rent-price").value;
+    const clothesPrice = document.getElementById("clothes-price").value;
 
-function getUpdate(click){
+    const totalPrice =
+      parseFloat(foodPrice) + parseFloat(rentPrice) + parseFloat(clothesPrice);
 
+    let expensesPrice = (document.getElementById("expenses-price").innerText =
+      totalPrice);
 
-    
+    let income = document.getElementById("income-input").value;
 
+    income = parseFloat(income) - parseFloat(expensesPrice);
 
+    let balancePrice = document.getElementById("balance-price");
 
-        document.getElementById(click).addEventListener('click',function(){
+    balancePrice.innerText = income;
+  });
 
+  document.getElementById(click).addEventListener("click", function () {
+    let saveInput = document.getElementById("save-input").value;
+    const savingAmount = document.getElementById("saving-amount");
 
-           
-                
-            
-
-
-            const foodPrice = document.getElementById('food-price').value;
-            const rentPrice = document.getElementById('rent-price').value;
-            const clothesPrice = document.getElementById('clothes-price').value;
-    
-        
-            const totalPrice = parseFloat(foodPrice)  + parseFloat(rentPrice) + parseFloat(clothesPrice);
-            
-        
-        
-            let expensesPrice = document.getElementById('expenses-price').innerText = totalPrice;
-            
-            let  income = document.getElementById('income-money').value;
-    
-            income =   parseFloat(income) - parseFloat(expensesPrice);
-    
-            let  balancePrice = document.getElementById('balance-price')  ;
-    
-            balancePrice.innerText =  income ;
-
-            
-    
-            document.getElementById(click).addEventListener('click',function(){
-    
-            })
-        
-    
-            let saveInput = document.getElementById('button-input').value;
-            const savingAmount = document.getElementById('saving-amount');
-    
-            savingAmount.innerText = saveInput / 10;
-            
-    
-            let remainingAmount = document.getElementById('remaining-amount').innerText  = parseFloat( balancePrice) - parseFloat( savingAmount) ;
-            
-    
-    
-        
-            
-    
-            
-            
-            
-            
-            
-               
-        })
-
-    
-
-    
-    
-
-    
-     
-
+    let myIncome = document.getElementById("income-input").value;
+    let userSaving = (saveInput * myIncome) / 100;
+        savingAmount.innerText = userSaving;
+    let remainingAmount = document.getElementById("remaining-amount");
+    let balancePrice = document.getElementById("balance-price").innerText;
+    let totalRemain = balancePrice - userSaving;
+    remainingAmount.innerText = totalRemain;
+  });
 }
 
-
-
-getUpdate('calc-button')  
-getUpdate('button-save')
-
-
-    
-
-
-
-    
-
-
-
-
+getUpdate("calc-button");
+getUpdate("button-save");
