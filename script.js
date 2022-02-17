3dcb6aeb840cb46c93f64e086b35c5250defc24d
Farhan910 <farhanfaruk221@gmail.com>
2022-02-17 23:00:06 +0600

    8th

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function getIncome() {
   let incomeInput = document.getElementById("income-input").value;
-  incomeInput= parseFloat(incomeInput);
+  incomeInput = parseFloat(incomeInput);
 
   if (isNaN(incomeInput) != 0 || incomeInput <= 0) {
     alert("Please give a valid income number !!");
@@ -10,7 +10,7 @@ function getIncome() {
     return incomeInput;
   }
 }
-// --------------<var-declination----------->
+// <--------------var-declination----------->
 function getClick(click) {
   document.getElementById(click).addEventListener("click", function () {
     const foodCost = document.getElementById("food-cost").value;
@@ -24,46 +24,42 @@ function getClick(click) {
       foodPrice = "";
     }
 
-    if (isNaN(rentCost) != 0 || rentCost <=0) {
+    if (isNaN(rentCost) != 0 || rentCost <= 0) {
       alert("Please give a valid Rent price!!");
       rentCost = "";
     }
-    if (isNaN(clothesCost) != 0 || clothesCost <=0) {
+    if (isNaN(clothesCost) != 0 || clothesCost <= 0) {
       alert("Please give a valid Clothes price !!");
       clothesCost = "";
     }
 
-    // <---------------counting the sum of Expenses -------------> 
+    // <---------------counting the sum of Expenses ------------->
 
-    const totalCostPrice = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+    const totalCostPrice =
+      parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
 
     let totalExpenses = (document.getElementById("total-expenses").innerText =
       totalCostPrice);
 
     let incomeInput = getIncome();
-
     let myBalance = incomeInput - parseFloat(totalExpenses);
 
-    
     let balanceOutput = document.getElementById("balance-output");
-    
     balanceOutput.innerText = myBalance;
-    
-    // <-----------------another error handling for total expenses -----------> 
-        if(incomeInput < totalExpenses){
-        alert ('Please dont waste your money what is out of your income !!!')
-        balanceOutput.innerText = 0;
+
+    // <-----------------another error handling for total expenses ----------->
+    if (incomeInput < totalExpenses) {
+      alert("Please dont waste your money what is out of your income !!!");
+      balanceOutput.innerText = 0;
     }
-    
   });
 
-//   <-------------------save money section------------------->
+  //   <-------------------save money section------------------->
   document.getElementById(click).addEventListener("click", function () {
     let saveInput = document.getElementById("save-input").value;
     const savingAmount = document.getElementById("saving-amount");
-    if (isNaN(saveInput) != 0  || saveInput <=0) {
+    if (isNaN(saveInput) != 0 || saveInput <= 0) {
       alert(" give a valid save input if you have to save your money ! ");
-      
     }
 
     // <-----------counting the save money percentage and get total to remaining ------------>
@@ -76,12 +72,9 @@ function getClick(click) {
     remainingAmount.innerText = totalRemain;
 
     // <-------------------another error handling for balance output --------------->
-    if(balanceOutput < userSaving){
-        alert ('Dont do this !!')
-        
-       
+    if (balanceOutput < userSaving) {
+      alert("Dont do this !!");
     }
-
   });
 }
 // <-------------calling the button as a parameter for the getUpdate function--------------->
